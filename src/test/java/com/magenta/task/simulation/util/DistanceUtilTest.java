76a9cc1ce17fc67bc5a35d89e067920ed8ca4f13
Валентин <moysait1984@yandex.ru>
2021-07-03 18:41:34 +0400

    add Distance test calc on two points

--- src/test/java/com/magenta/task/simulation/util/DistanceUtilTest.java
+++ src/test/java/com/magenta/task/simulation/util/DistanceUtilTest.java
@@ -1,6 +1,7 @@
 package com.magenta.task.simulation.util;
 
 import com.magenta.task.simulation.dataMock.DistanceData;
+import com.magenta.task.simulation.dataMock.ScheduleData;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -9,15 +10,26 @@ import org.springframework.boot.test.context.SpringBootTest;
 class DistanceUtilTest {
 
     @Test
-    void calculate() {
-        Double actualDouble = DistanceUtil.calculate(
+    void calcForTwoLatitudeAndLongitude() {
+        double actualDouble = DistanceUtil.calculate(
                 DistanceData.LATITUDE1, DistanceData.LONGITUDE1,
-                DistanceData.LATITUDE2, DistanceData.LONGITUDE2
-        );
+                DistanceData.LATITUDE2, DistanceData.LONGITUDE2);
 
         long actual = Math.round(actualDouble);
 
         long expected = Math.round(DistanceData.DISTANCE_BETWEEN_POINTS);
+
+        Assertions.assertEquals(expected, actual);
+
+    }
+
+    @Test
+    void calcDistanceOnTwoPoint() {
+        double actualDouble = DistanceUtil.calculate(ScheduleData.DC, ScheduleData.POINT1);
+
+        long actual = Math.round(actualDouble);
+
+        long expected = 10;
 
         Assertions.assertEquals(expected, actual);
 
