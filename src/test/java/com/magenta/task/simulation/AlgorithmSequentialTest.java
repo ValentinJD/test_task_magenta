ca8a95700991e50e2711d84cf9b9b24c671f0b1c
Валентин <moysait1984@yandex.ru>
2021-07-03 19:43:13 +0400

    add AlgorithmSequentialTest

---
+++ src/test/java/com/magenta/task/simulation/AlgorithmSequentialTest.java
@@ -0,0 +1,29 @@
+package com.magenta.task.simulation;
+
+import com.magenta.task.simulation.dataMock.ScheduleData;
+import com.magenta.task.simulation.model.Schedule;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.time.LocalTime;
+
+@SpringBootTest
+class AlgorithmSequentialTest {
+
+    @Autowired
+    SimulationAlgorithmSequential algorithmSequential;
+
+    @Test
+    void simulate() {
+        Schedule actual = algorithmSequential.simulate(ScheduleData.getOrders(), ScheduleData.UAZ_452,
+                LocalTime.of(8,0), LocalTime.of(22,0));
+
+        Schedule expected = ScheduleData.SCHEDULE;
+
+        Assertions.assertIterableEquals(expected.getWorkList(), actual.getWorkList());
+        Assertions.assertEquals(expected.getTimeGoToOutDC(), actual.getTimeGoToOutDC());
+        Assertions.assertEquals(expected.getTimeToReturnInDC(), actual.getTimeToReturnInDC());
+    }
+}
